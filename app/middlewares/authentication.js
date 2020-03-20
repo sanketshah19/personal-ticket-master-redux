8ae5eabc10d5d6e1064087bb9d996311673791d7
sanketshah19 <sanketshah17.ss@gmail.com>
2020-03-20 17:40:14 +0530

    Feat: Add authenticateUser middleware

---
+++ app/middlewares/authentication.js
@@ -0,0 +1,20 @@
+const {User} = require('../models/user')
+
+const authenticateUser = function(req, res, next){
+    const token = req.header('x-auth')
+    User.findByToken(token)
+        .then((user) => {
+            if(user){
+                req.user = user
+                req.token = token
+                next()
+            }else{
+                res.status('401').send({notice: 'Token not availabel'})
+            }
+        })
+        .catch((err) => {
+            res.status('401').send(err)
+        })
+}
+
+module.exports = authenticateUser
