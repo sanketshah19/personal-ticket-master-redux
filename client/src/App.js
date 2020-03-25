08893a95cc83325f9ea3e538bde0a5431855eccf
sanketshah19 <sanketshah17.ss@gmail.com>
2020-03-25 14:04:17 +0530

    Initialize App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import {Nav, Navbar} from 'react-bootstrap';
+import {BrowserRouter, Switch, Link, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-      <h2>Ticket Master</h2>
+    <div className="container-fluid">
+      <BrowserRouter>
+      
+      {
+        localStorage.getItem('authToken') ?
+        (
+          <Navbar bg="dark" variant="dark" expand="lg">
+              <Navbar.Brand href="/">Ticket-Master</Navbar.Brand>
+              <Nav.Item className="ml-auto">
+                <Link to="/" className="ml-2">Home</Link>
+                <Link to="/customers" className="ml-2">Customers</Link>
+                <Link to="/departments" className="ml-2">Departments</Link>
+                <Link to="/employees" className="ml-2">Employees</Link>
+                <Link to="/tickets" className="ml-2">Tickets</Link>
+                <Link to="#" className="ml-2">Logout</Link>
+              </Nav.Item>
+          </Navbar>
+        )
+        :
+        (
+          <Navbar bg="dark" variant="dark" expand="lg">
+              <Navbar.Brand href="/">Ticket-Master</Navbar.Brand>
+              <Nav.Item className="ml-auto">
+                <Link to="/" className="ml-2">Home</Link>
+                <Link to="/users/register" className="ml-2">Register</Link>
+                <Link to="/users/login" className="ml-2">Login</Link>
+              </Nav.Item>
+          </Navbar>
+        )
+      }
+
+      </BrowserRouter>
     </div>
   )
 }
